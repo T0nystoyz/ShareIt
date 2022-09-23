53381bf2cf99ae7b9e0d6e2ffa7e6c905a85a3bb
Anton <tonystoyz@yandex.ru>
2022-09-23 13:07:43 +0300

    работа с H2

--- src/main/java/ru/practicum/shareit/user/repository/UserDAO.java
+++ src/main/java/ru/practicum/shareit/user/repository/UserDAO.java
@@ -1,6 +1,7 @@
 package ru.practicum.shareit.user.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.shareit.user.model.User;
@@ -10,5 +11,7 @@ public interface UserDAO extends JpaRepository<User, Long> {
     @Query("select u from User u where u.id = ?1")
     User findById(long id);
 
+    @Modifying
+    @Query("delete from User u where u.id = ?1")
     void deleteUserById(long id);
 }
